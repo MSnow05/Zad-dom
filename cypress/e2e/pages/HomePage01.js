932036b0d3c752be7817bb8a1d656506792d6bca
msnow05 <140186070+MSnow05@users.noreply.github.com>
2024-03-10 10:29:56 +0100

    e

--- cypress/e2e/pages/HomePage01.js
+++ cypress/e2e/pages/HomePage01.js
@@ -1,7 +1,9 @@
 class HomePage {
     goToCourseHomepage() {
         cy.get('[id="go-to-course-homepage-widget"]').scrollIntoView().should("be.visible").wait(5000);
-        cy.get('button[class="next-7afvtf e1phyiqy6"]').click().wait(5000);
+    }
+    proceedToNext() {   
+    cy.get('button[class="next-7afvtf e1phyiqy6"]').click().wait(5000);
     }
 
     logOut() {
