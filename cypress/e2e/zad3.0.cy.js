1ef68c350af4d6f4791c6237d0021ad5961f74dd
msnow05 <140186070+MSnow05@users.noreply.github.com>
2024-03-11 10:16:34 +0100

    e

--- cypress/e2e/zad3.0.cy.js
+++ cypress/e2e/zad3.0.cy.js
@@ -1,5 +1,7 @@
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage01';
+import HomePage from './pages/HomePage01';
 
 describe('User can visit GoIT page', () => {
     const loginPage = new LoginPage();
@@ -11,6 +13,12 @@ describe('User can visit GoIT page', () => {
 
     it('successfully login to the GoIT page first test', () => {
         loginPage.logInUser('user888@gmail.com', '1234567890');
+        homePage.goToCourseHomepage();
+        homePage.logOut();
+    });
+
+    it('successfully login to the Example page', () => {
+        loginPage.logInUser('test@example.com', 'password123');
         homePage.goToCourseHomepage();
         homePage.logOut();
     });
